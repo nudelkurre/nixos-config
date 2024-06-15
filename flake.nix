bf281148ee7cb1e679b52bc0207012db4b73b3be
Emil Wendin <nudelkurre@protonmail.com>
2024-06-16 00:29:51 +0200

    Moved all pkg derivations from configs into self flake

--- flake.nix
+++ flake.nix
@@ -118,6 +118,64 @@
                 cp -v wallhaven-z8xqqo.jpg $out/share/wallpapers/wallhaven-z8xqqo.jpg
                 '';
             };
+
+            mangadex-downloader = pkgs.python311Packages.buildPythonPackage rec {
+                pname = "mangadex-downloader";
+                version = "2.10.3";
+                src = pkgs.fetchPypi {
+                    inherit pname version;
+                    sha256 = "sha256-RM1UCnU7/P913g7HfTCZp166/0msK3OkfExJd9BCpOs=";
+                };
+
+                dependencies = with pkgs.python311Packages; [
+                    requests
+                    (
+                        buildPythonPackage rec {
+                            pname = "requests-doh";
+                            version = "0.3.3";
+                            src = pkgs.fetchPypi {
+                                inherit pname version;
+                                sha256 = "sha256-P11hy9sodrp7ERRCy/5bMlYfLbkWb/MnhxGTfrPWYuE=";
+                            };
+
+                            propagatedBuildInputs = [
+                                requests
+                                dnspython
+                            ];            
+
+                            doCheck = false;
+                        }
+                    )
+                    pysocks
+                    tqdm
+                    pathvalidate
+                    packaging
+                    pyjwt
+                    beautifulsoup4
+                    pillow
+                    chardet
+                ];
+
+                doCheck = false;
+            };
+
+            pkg2zip = pkgs.stdenv.mkDerivation rec {
+                pname = "pkg2zip";
+                version = "2.3";
+                src = pkgs.fetchurl {
+                    url = "https://github.com/lusid1/pkg2zip/archive/${version}.tar.gz";
+                    sha256 = "sha256-N4D3j9GCMI7XhUxpx6hcTG1Plx7jqw5Zj1htz37imhU=";
+                };
+
+                buildPhase = ''
+                    make CFLAGS="-DNDEBUG -O2 -Wno-format-truncation"
+                '';
+
+                installPhase = ''
+                    mkdir -p $out/bin
+                    install -Dm755 pkg2zip $out/bin/pkg2zip
+                '';
+            };
         };
     };
 }
