185704a9c4adc7b7c08891d20c1e76327052c7a3
Emil Wendin <nudelkurre@protonmail.com>
2024-06-16 00:30:34 +0200

    Added mangadex-dl and pkg2zip from self flake packages

--- home-manager/home-desktop.nix
+++ home-manager/home-desktop.nix
@@ -58,29 +58,13 @@
         keybase-gui
         lm_sensors
         m4b-tool.m4b-tool-libfdk
+        mypkgs.mangadex-downloader
         mate.mate-polkit
         mkvtoolnix
         pavucontrol
+        mypkgs.pkg2zip
         scripts.video-dl
         wl-clipboard
-
-        (pkgs.stdenv.mkDerivation rec {
-                pname = "pkg2zip";
-                version = "2.3";
-                src = pkgs.fetchurl {
-                    url = "https://github.com/lusid1/pkg2zip/archive/${version}.tar.gz";
-                    sha256 = "sha256-N4D3j9GCMI7XhUxpx6hcTG1Plx7jqw5Zj1htz37imhU=";
-                };
-
-                buildPhase = ''
-                    make CFLAGS="-DNDEBUG -O2 -Wno-format-truncation"
-                '';
-
-                installPhase = ''
-                    mkdir -p $out/bin
-                    install -Dm755 pkg2zip $out/bin/pkg2zip
-                '';
-            })
     ];
 
     qt = {
