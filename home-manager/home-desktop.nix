c6506616630f14d188831c349bf79fc629c1d665
Emil Wendin <nudelkurre@protonmail.com>
2024-08-12 11:36:02 +0200

    Set which program to focus at launch with new submodules

--- home-manager/home-desktop.nix
+++ home-manager/home-desktop.nix
@@ -177,15 +177,25 @@
     workspaces = [
         {
             name = "1";
-            programs = [ "Alacritty" "kitty" ];
+            # programs = [ "Alacritty" "kitty" ];
+            programs = [
+                {name = "Alacritty"; focus = true;}
+                {name = "kitty"; focus = true;}
+            ];
         }
         {
             name = "2";
-            programs = [ "firefox" ];
+            # programs = [ "firefox" ];
+            programs = [
+                {name = "firefox"; focus = true;}
+            ];
         }
         {
             name = "3";
-            programs = [ "FreeTube" ];
+            # programs = [ "FreeTube" ];
+            programs = [
+                {name = "FreeTube"; focus = true;}
+            ];
         }
         {
             name = "4";
@@ -193,7 +203,12 @@
         }
         {
             name = "5";
-            programs = [ "steam" "gamescope" "com.usebottles.bottles" ];
+            # programs = [ "steam" "gamescope" "com.usebottles.bottles" ];
+            programs = [
+                {name = "Steam";}
+                {name = "gamescope";}
+                {name = "com.usebottles.bottles";}
+            ];
         }
         {
             name = "6";
@@ -201,7 +216,13 @@
         }
         {
             name = "7";
-            programs = [ "Keybase" "discord" "chatterino" "com.chatterino.chatterino" ];
+            # programs = [ "Keybase" "discord" "chatterino" "com.chatterino.chatterino" ];
+            programs = [
+                {name = "Keybase";}
+                {name = "discord";}
+                {name = "chatterino";}
+                {name = "com.chatterino.chatterino";}
+            ];
         }
         {
             name = "8";
@@ -209,11 +230,14 @@
         }
         {
             name = "9";
-            programs = [ "pavucontrol" "org.freedesktop.ryuukyu.Helvum" ];
+            # programs = [ "pavucontrol" "org.freedesktop.ryuukyu.Helvum" ];
         }
         {
             name = "10";
-            programs = [ "VSCodium" ];
+            # programs = [ "VSCodium" ];
+            programs = [
+                {name = "VSCodium"; focus = true;}
+            ];
         }
     ];
 
--- home-manager/home-laptop.nix
+++ home-manager/home-laptop.nix
@@ -150,15 +150,22 @@
     workspaces = [
         {
             name = "1";
-            programs = [ "Alacritty" "kitty" ];
+            programs = [
+                {name ="Alacritty"; focus = true;}
+                {name ="kitty"; focus = true;}
+            ];
         }
         {
             name = "2";
-            programs = [ "firefox" ];
+            programs = [
+                {name = "firefox"; focus = true;}
+            ];
         }
         {
             name = "3";
-            programs = [ "FreeTube" ];
+            programs = [
+                {name = "FreeTube"; focus = true;}
+            ];
         }
         {
             name = "4";
@@ -166,7 +173,11 @@
         }
         {
             name = "5";
-            programs = [ "steam" "gamescope" "com.usebottles.bottles" ];
+            programs = [
+                {name = "steam";}
+                {name = "gamescope";}
+                {name = "com.usebottles.bottles";}
+            ];
         }
         {
             name = "6";
@@ -174,15 +185,25 @@
         }
         {
             name = "7";
-            programs = [ "Keybase" "discord" "chatterino" "com.chatterino.chatterino" ];
+            programs = [
+                {name = "Keybase";}
+                {name = "discord";}
+                {name = "chatterino";}
+                {name = "com.chatterino.chatterino";}
+            ];
         }
         {
             name = "8";
-            programs = [ "VSCodium" ];
+            programs = [
+                {name = "VSCodium";}
+            ];
         }
         {
             name = "9";
-            programs = [ "pavucontrol" "org.freedesktop.ryuukyu.Helvum" ];
+            programs = [
+                {name = "pavucontrol";}
+                {name = "org.freedesktop.ryuukyu.Helvum";}
+            ];
         }
         {
             name = "10";
