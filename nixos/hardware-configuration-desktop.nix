996f880ec8e18404abf27e56ffcde59293617262
Emil Wendin <nudelkurre@protonmail.com>
2024-08-23 17:30:46 +0200

    Added config to add virtual camera in OBS

--- nixos/hardware-configuration-desktop.nix
+++ nixos/hardware-configuration-desktop.nix
@@ -11,7 +11,10 @@
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
     boot.initrd.kernelModules = [ "amdgpu" ];
     boot.kernelModules = [ "kvm-intel" ];
-    boot.extraModulePackages = [ ];
+    boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+    boot.extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS CAM" exclusive_caps=1
+    '';
 
     fileSystems = {
         "/" = {
--- nixos/hardware-configuration-laptop.nix
+++ nixos/hardware-configuration-laptop.nix
@@ -11,7 +11,10 @@
     boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
     boot.kernelModules = [ "kvm-intel" ];
-    boot.extraModulePackages = [ ];
+    boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+    boot.extraModprobeConfig = ''
+        options v4l2loopback devices=1 video_nr=1 card_label="OBS CAM" exclusive_caps=1
+    '';
 
     fileSystems = {
         "/" = {
