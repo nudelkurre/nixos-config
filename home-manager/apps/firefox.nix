3c72d17cab38092e185fad9dcfbf0c5c6135886b
Emil Wendin <nudelkurre@protonmail.com>
2024-08-24 21:12:53 +0200

    Changed from firefox-esr-128 to firefox-esr

--- home-manager/apps/firefox.nix
+++ home-manager/apps/firefox.nix
@@ -2,7 +2,7 @@
 {
     programs.firefox = {
         enable = true;
-        package = pkgs.firefox-esr-128;
+        package = pkgs.firefox-esr;
         profiles = {
             "Default-lw" = {
                 isDefault = true;
