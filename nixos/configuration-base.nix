f08322aed1e985333ca71b9060e62db6d118d7ba
Emil Wendin <nudelkurre@protonmail.com>
2024-11-05 14:30:35 +0100

    Removed html documentations since not used

--- nixos/configuration-base.nix
+++ nixos/configuration-base.nix
@@ -123,6 +123,10 @@
         };
     };
 
+    documentation = {
+        doc.enable = false;
+    };
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It's perfectly fine and recommended to leave
