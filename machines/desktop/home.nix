4c5325eb62720bc8922812ac85a8d65497254422
Emil Wendin <nudelkurre@protonmail.com>
2025-03-07 16:13:13 +0100

    Added .desktop file for steam with flag for disable minimize to tray

--- machines/desktop/home.nix
+++ machines/desktop/home.nix
@@ -251,6 +251,18 @@
     ];
 
     xdg = {
+        desktopEntries = {
+            steam = {
+                name = "Steam";
+                comment = "Application for managing and playing games on Steam";
+                exec = "steam %U --no-minimize-to-tray";
+                icon = "steam";
+                terminal = false;
+                type = "Application";
+                categories = ["Network" "FileTransfer" "Game"];
+                mimeType = ["x-scheme-handler/steam" "x-scheme-handler/steamlink"];
+            };
+        };
         mimeApps = {
             enable = true;
             defaultApplications = {
