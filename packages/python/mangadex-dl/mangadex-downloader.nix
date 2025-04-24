33496d30d534a8af3d136978d01662f20ceb6acd
Emil Wendin <nudelkurre@protonmail.com>
2025-04-24 17:21:26 +0200

    Updated mangadex-downloader to 3.1.3

--- packages/python/mangadex-dl/mangadex-downloader.nix
+++ packages/python/mangadex-dl/mangadex-downloader.nix
@@ -1,35 +1,15 @@
 {pkgs, buildPythonPackage, fetchPypi, ...}:
 buildPythonPackage rec {
-  pname = "mangadex-downloader";
-  version = "2.10.3";
+  pname = "mangadex_downloader";
+  version = "3.1.3";
   src = fetchPypi {
     inherit pname version;
-    sha256 = "sha256-RM1UCnU7/P913g7HfTCZp166/0msK3OkfExJd9BCpOs=";
+    sha256 = "sha256-LmxR9VQGyLrHbza/CeXwoNf02ywnqO7JFu0wmPF5PUM=";
   };
-
-  patches = [
-    ./mangadex-dl.patch
-  ];
 
   dependencies = with pkgs.python3Packages; [
     requests
-    (
-      buildPythonPackage rec {
-        pname = "requests-doh";
-        version = "0.3.3";
-        src = fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-P11hy9sodrp7ERRCy/5bMlYfLbkWb/MnhxGTfrPWYuE=";
-        };
-
-        propagatedBuildInputs = [
-          requests
-          dnspython
-        ];            
-
-        doCheck = false;
-      }
-    )
+    pkgs.mypkgs.requests-doh
     pysocks
     tqdm
     pathvalidate
