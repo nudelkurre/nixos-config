483a8d208ab67bdfea4afb11602cb6876b6fafc5
Emil Wendin <nudelkurre@protonmail.com>
2025-05-31 02:11:13 +0200

    Fixed renaming of nerd-fonts

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -81,7 +81,8 @@
         packages = with pkgs; [
             freefont_ttf
             monaspace
-            (nerdfonts.override { fonts = [ "Noto" "Monaspace" ]; })
+            nerd-fonts.monaspace
+            nerd-fonts.noto
             noto-fonts
             noto-fonts-cjk-sans
             noto-fonts-emoji
