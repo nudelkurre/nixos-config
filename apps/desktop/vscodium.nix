873f93bdc7eeeb950fe6a0b7628a643dcf9762f6
Emil Wendin <nudelkurre@protonmail.com>
2025-05-31 02:06:27 +0200

    Fixed renaming of profile settings for vscodium

--- apps/desktop/vscodium.nix
+++ apps/desktop/vscodium.nix
@@ -4,35 +4,39 @@
 {
     programs.vscode = {
         enable = ! config.disable.vscodium;
-        enableUpdateCheck = false;
-        extensions = with pkgs.vscode-extensions; [
-            golang.go
-            james-yu.latex-workshop
-            jnoortheen.nix-ide
-            ms-dotnettools.csdevkit
-            ms-python.python
-            naumovs.color-highlight
-            catppuccin.catppuccin-vsc
-            catppuccin.catppuccin-vsc-icons
-        ];
-        keybindings = [
-            {
-                "command" = "workbench.action.terminal.new";
-                "key" = "ctrl+alt+t";
-            }
-            {
-                "command" = "editor.action.commentLine";
-                "key" = "ctrl+shift+7";
-                "when" = "editorTextFocus && !editorReadonly";
-            }
-        ];
         package = pkgs.unstable.vscodium;
-        userSettings = {
-            "editor.fontFamily" = "${config.fonts.name}";
-            "editor.insertSpaces" = true;
-            "editor.tabSize" = 4;
-            "workbench.colorTheme" = "Catppuccin Frappé";
-            "workbench.iconTheme" = "catppuccin-frappe";
+        profiles = {
+            "default" = {
+                enableUpdateCheck = false;
+                extensions = with pkgs.vscode-extensions; [
+                    golang.go
+                    james-yu.latex-workshop
+                    jnoortheen.nix-ide
+                    ms-dotnettools.csdevkit
+                    ms-python.python
+                    naumovs.color-highlight
+                    catppuccin.catppuccin-vsc
+                    catppuccin.catppuccin-vsc-icons
+                ];
+                keybindings = [
+                    {
+                        "command" = "workbench.action.terminal.new";
+                        "key" = "ctrl+alt+t";
+                    }
+                    {
+                        "command" = "editor.action.commentLine";
+                        "key" = "ctrl+shift+7";
+                        "when" = "editorTextFocus && !editorReadonly";
+                    }
+                ];
+                userSettings = {
+                    "editor.fontFamily" = "${config.fonts.name}";
+                    "editor.insertSpaces" = true;
+                    "editor.tabSize" = 4;
+                    "workbench.colorTheme" = "Catppuccin Frappé";
+                    "workbench.iconTheme" = "catppuccin-frappe";
+                };
+            };
         };
     };
 }
