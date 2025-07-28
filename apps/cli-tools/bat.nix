acfb549e4d0ae8fe7bb04911081f29f3e83b35c4
Emil Wendin <nudelkurre@protonmail.com>
2025-07-28 21:00:30 +0200

    Added catppuccin theme to bat

--- apps/cli-tools/bat.nix
+++ apps/cli-tools/bat.nix
@@ -2,8 +2,19 @@
 {
     programs.bat = {
         config = {
-
+            style = "numbers,rule";
+            theme = "Catppuccin Frappe";
         };
         enable = ! config.disable.bat;
+        themes = {
+            catppuccin = {
+                src = pkgs.fetchFromGitHub {
+                    owner = "catppuccin";
+                    repo = "bat";
+                    rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
+                    hash = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
+                };
+            };
+        };
     };
 }
