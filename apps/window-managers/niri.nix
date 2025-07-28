926568885fe3551ef8354058407e7429a99f11fd
Emil Wendin <nudelkurre@protonmail.com>
2025-07-28 21:06:51 +0200

    Added to launch swww-background at niri startup

--- apps/window-managers/niri.nix
+++ apps/window-managers/niri.nix
@@ -403,5 +403,6 @@ overview {
 spawn-at-startup "${pkgs.dbus}/bin/dbus-update-activation-environment" "--systemd" "DISPLAY" "WAYLAND_DISPLAY" "SWAYSOCK" "XDG_CURRENT_DESKTOP" "XDG_SESSION_TYPE" "NIXOS_OZONE_WL" "XCURSOR_THEME" "XCURSOR_SIZE"
 spawn-at-startup "${pkgs.xwayland-satellite}/bin/xwayland-satellite"
 spawn-at-startup "${pkgs.ngb}/bin/ngb"
+spawn-at-startup "swww-background"
     '';
 }
