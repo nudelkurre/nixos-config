12a5c048a8bfa2c9a96b08da728c6a37f0c986b1
Emil Wendin <nudelkurre@protonmail.com>
2025-08-20 22:23:47 +0200

    Added service and timer to change to random wallpaper every 15 min

--- apps/services/swww.nix
+++ apps/services/swww.nix
@@ -1,19 +1,48 @@
 {pkgs, config, lib, ...}:
 {
-    home.packages = [
-        (pkgs.writeShellApplication {
-            name = "swww-background";
-            runtimeInputs = [
-                pkgs.swww
-            ];
-            text = lib.strings.concatStringsSep "\n" (map
+    home.file = {
+        "swww-background" = {
+            enable = true;
+            executable = true;
+            target = ".local/bin/swww-background";
+            text = ''#!/usr/bin/env bash
+            '' + lib.strings.concatStringsSep "\n" (map
             (m:
                 ''
-                    swww img -o ${m.name} --resize=crop --transition-type=random "$(find ${pkgs.mypkgs.wallpapers}/share/wallpapers/${m.orientation} -maxdepth 1 -type f | shuf -n 1)"
+                    ${config.services.swww.package}/bin/swww img -o ${m.name} --resize=crop --transition-type=fade "$(find ${pkgs.mypkgs.wallpapers}/share/wallpapers/${m.orientation} -maxdepth 1 -type f | shuf -n 1)"
                 ''
             )
             (config.monitors.outputs));
-        })
-    ];
-    services.swww.enable = true;
-}
+        };
+    };
+    services.swww = {
+        enable = true;
+        package = pkgs.unstable.swww;
+    };
+    systemd.user = {
+        services = {
+            bgchange = {
+                Service = {
+                    ExecStart = "${config.home.homeDirectory}/.local/bin/swww-background";
+                    Type = "oneshot";
+                };
+                Unit = {
+                    Description = "Change background";
+                };
+            };
+        };
+        timers = {
+            bgchange = {
+                Install = {
+                    WantedBy = ["timers.target"];
+                };
+                Timer = {
+                    OnCalendar = "*-*-* *:0/15:*";
+                    Unit = "bgchange.service";
+                };
+                Unit = {
+                    Description = "Change background";
+                };
+            };
+        };
+    };
