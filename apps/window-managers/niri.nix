dcdecfcee95c841de1f0299c77f9578cea8f98a5
Emil Wendin <nudelkurre@protonmail.com>
2025-08-30 15:44:27 +0200

    Removed workspace shadows from niri when in overview

--- apps/window-managers/niri.nix
+++ apps/window-managers/niri.nix
@@ -498,6 +498,9 @@ in
 
         overview {
             zoom 0.75
+            workspace-shadow {
+                off
+            }
         }
 
         spawn-at-startup "${pkgs.dbus}/bin/dbus-update-activation-environment" "--systemd" "DISPLAY" "WAYLAND_DISPLAY" "SWAYSOCK" "XDG_CURRENT_DESKTOP" "XDG_SESSION_TYPE" "NIXOS_OZONE_WL" "XCURSOR_THEME" "XCURSOR_SIZE"
