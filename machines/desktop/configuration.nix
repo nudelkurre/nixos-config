1412f0dee3471c9d637f286725effe2d72dec57e
Emil Wendin <nudelkurre@protonmail.com>
2025-09-13 00:15:46 +0200

    Added substituters for extra binary caches to desktop

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -165,6 +165,13 @@
                 "nix-command"
                 "flakes"
             ];
+            substituters = [
+                "https://cache.nixos.org/"
+                "https://nix-community.cachix.org"
+            ];
+            trusted-public-keys = [
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            ];
         };
     };
 
