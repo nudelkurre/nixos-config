33e4418f8652744394213b26a29c76f7ca41aada
Emil Wendin <nudelkurre@protonmail.com>
2025-10-21 18:02:05 +0200

    Removed video specific settings for image wallpapers

--- apps/services/mpvpaper.nix
+++ apps/services/mpvpaper.nix
@@ -21,7 +21,7 @@ in
                                 if m.orientation == "horizontal" then
                                     "${pkgs.mpvpaper}/bin/mpvpaper ${m.name} ${pkgs.wallpapers.video-wallpapers}/share/wallpapers/${m.orientation} --slideshow 1200 --mpv-options 'shuffle panscan=1.0 really-quiet no-audio' --auto-pause"
                                 else
-                                    "${pkgs.mpvpaper}/bin/mpvpaper ${m.name} ${pkgs.wallpapers.wallpapers}/share/wallpapers/${m.orientation} --slideshow 1200 --mpv-options 'shuffle panscan=1.0 really-quiet no-audio' --auto-pause";
+                                    "${pkgs.mpvpaper}/bin/mpvpaper ${m.name} ${pkgs.wallpapers.wallpapers}/share/wallpapers/${m.orientation} --slideshow 1200 --mpv-options 'shuffle panscan=1.0 really-quiet'";
                             Restart = "always";
                             RestartSec = "5s";
                         };
