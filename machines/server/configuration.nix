f246329230d85db58d82f2f1c6baf8f193ab7b34
Emil Wendin <nudelkurre@protonmail.com>
2026-01-18 15:41:41 +0100

    Enabled hardware acceleration for server

--- machines/server/configuration.nix
+++ machines/server/configuration.nix
@@ -101,6 +101,9 @@
         graphics = {
             enable = true;
             enable32Bit = true;
+            extraPackages = with pkgs; [
+                intel-media-driver
+            ];
         };
     };
 
