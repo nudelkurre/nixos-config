d38b260fd86540d866591b306aa110450e2f2707
Emil Wendin <nudelkurre@protonmail.com>
2026-01-24 16:07:20 +0100

    Changed some settings in mangohud to show cpu and gpu info

--- apps/utils/mangohud.nix
+++ apps/utils/mangohud.nix
@@ -8,15 +8,18 @@
             frametime = false;
             cpu_temp = true;
             cpu_mhz = true;
-            cpu_power = false;
-            gpu_stats = false; # Causes stuttering in game if enabled
-            gpu_temp = false;
-            gpu_power = false;
+            cpu_power = true;
+            gpu_stats = true;
+            gpu_temp = true;
+            gpu_power = true;
             gpu_name = true;
+            gpu_list = 1; # Set to only use the igpu to fix no temp showing on cpu and not make gpu stutter
             throttling_status = false;
-            vram = false;
+            vram = true;
             ram = true;
+            swap = true;
             gamemode = true;
+            engine_short_names = true;
 
             fps_limit = "60,30,0";
             font_size = "20";
@@ -33,7 +36,7 @@
 
             mangoapp_steam = true;
 
-            time_format = "%H:%M";
+            time_format = "%H:%M %Z";
         };
     };
 }
