87fcbd8902e5ea895e47cc12d8d5660da88ffb39
Emil Wendin <nudelkurre@protonmail.com>
2026-02-12 16:55:04 +0100

    Updated dunst to use main monitor settings when setting size

--- apps/services/dunst.nix
+++ apps/services/dunst.nix
@@ -1,32 +1,35 @@
-{ config, sharedSettings, ... }:
+{ config, lib, sharedSettings, ... }:
+let
+    main_monitor = builtins.head (lib.filter (output: output.name == config.monitors.primary) config.monitors.outputs);
+in
 {
     services.dunst = {
         enable = true;
         settings = {
             global = {
                 alignment = "center";
-                always_run_script = "true";
-                browser = "/usr/bin/firefox --private-window";
+                always_run_script = true;
+                browser = "${config.programs.firefox.package}/bin/firefox --private-window";
                 class = "Dunst";
-                corner_radius = 15;
+                corner_radius = config.desktop.corner-radius;
                 ellipsize = "middle";
                 font = "${config.fonts.name} ${toString config.fonts.size}";
                 format = "<b>%s</b>\\n%b";
                 frame_color = sharedSettings.colors.main;
-                frame_width = 1;
-                height = "(0, 500)";
-                hide_duplicate_count = "false";
-                history_length = 20;
+                frame_width = 2;
+                height = "(0, ${toString (builtins.floor (main_monitor.height / 3))})";
+                hide_duplicate_count = false;
+                history_length = 7;
                 horizontal_padding = 8;
                 icon_path = "/usr/share/icons/gnome/16x16/status/:/usr/share/icons/gnome/16x16/devices/";
                 icon_position = "left";
                 idle_threshold = 0;
-                ignore_newline = "no";
-                indicate_hidden = "yes";
+                ignore_newline = false;
+                indicate_hidden = true;
                 line_height = 0;
                 markup = "full";
-                max_icon_size = 32;
-                monitor = config.monitors.primary;
+                max_icon_size = 64;
+                monitor = main_monitor.name;
                 mouse_left_click = "close_current";
                 mouse_middle_click = "do_action";
                 mouse_right_click = "close_all";
@@ -34,16 +37,16 @@
                 padding = 8;
                 separator_color = "frame";
                 separator_height = 2;
-                shrink = "no";
+                shrink = false;
                 show_age_threshold = -1;
-                show_indicators = "no";
-                sort = "yes";
-                stack_duplicates = "true";
-                sticky_history = "no";
+                show_indicators = false;
+                sort = "urgency_descending";
+                stack_duplicates = true;
+                sticky_history = false;
                 title = "Dunst";
                 transparency = 0;
-                width = 300;
-                word_wrap = "yes";
+                width = "(${toString (builtins.floor (main_monitor.width / 6))}, ${toString (builtins.floor (main_monitor.width / 4))}";
+                word_wrap = true;
             };
             urgency_critical = {
                 background = sharedSettings.colors.red;
