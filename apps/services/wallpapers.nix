59d0265c2a02aac642a584d1063f280c35bb98fe
Emil Wendin <nudelkurre@protonmail.com>
2026-03-20 19:48:08 +0100

    Changed some bgchange scripts to be able to do smoother transitions when changing wallpaper

--- apps/services/wallpapers.nix
+++ apps/services/wallpapers.nix
@@ -7,11 +7,12 @@
 let
     target = "graphical-session.target";
     services = (
-        builtins.listToAttrs
-            (builtins.map (m: {
+        builtins.listToAttrs (
+            builtins.map (m: {
                 name = "bgchange-${m.name}";
                 value =
                     let
+                        mpv-socket = "/tmp/mpv-${m.name}.socket";
                         orientation = if m.transform == 0 || m.transform == 180 then "horizontal" else "vertical";
                     in
                     if m.wallpaper == "mpvpaper" && orientation == "horizontal" then
@@ -20,7 +21,16 @@ let
                                 WantedBy = [ target ];
                             };
                             Service = {
-                                ExecStart = "${pkgs.mpvpaper}/bin/mpvpaper ${m.name} ${pkgs.wallpapers.video-wallpapers}/share/wallpapers/${orientation} --mpv-options 'shuffle loop-file=inf panscan=1.0 really-quiet no-audio' --auto-pause";
+                                ExecStart = pkgs.writeShellScript "mpvpaper-${m.name}-start" ''
+                                    ${pkgs.mpvpaper}/bin/mpvpaper ${m.name} ${pkgs.wallpapers.video-wallpapers}/share/wallpapers/${orientation} --mpv-options 'shuffle loop-file=inf panscan=1.0 really-quiet no-audio input-ipc-server=${mpv-socket}' --auto-pause
+                                '';
+                                ExecReload = pkgs.writeShellScript "mpvpaper-${m.name}-reload" ''
+                                    echo 'playlist-next' | ${pkgs.socat}/bin/socat - ${mpv-socket}
+                                '';
+                                ExecStop = pkgs.writeShellScript "mpvpaper-${m.name}-stop" ''
+                                    echo 'quit' | ${pkgs.socat}/bin/socat - ${mpv-socket}
+                                    rm -v ${mpv-socket}
+                                '';
                                 Restart = "always";
                                 RestartSec = "5s";
                             };
@@ -33,36 +43,58 @@ let
                         }
                     else if m.wallpaper == "swww" then
                         {
+                            Install = {
+                                WantedBy = [ target ];
+                            };
                             Service = {
-                                ExecStart = pkgs.writeShellScript "bgchange-${m.name}" "${config.services.swww.package}/bin/swww img -o ${m.name} --resize=crop --transition-type=fade $(find ${pkgs.wallpapers.wallpapers}/share/wallpapers/${orientation} | grep -P '(png|jpg|jpeg)' | shuf -n 1)";
-                                ExecReload = pkgs.writeShellScript "bgchange-${m.name}" "${config.services.swww.package}/bin/swww img -o ${m.name} --resize=crop --transition-type=fade $(find ${pkgs.wallpapers.wallpapers}/share/wallpapers/${orientation} | grep -P '(png|jpg|jpeg)' | shuf -n 1)";
-                                ExecStopPost =  pkgs.writeShellScript "bgchange-${m.name}-clear" "${config.services.swww.package}/bin/swww clear -o ${m.name}";
+                                ExecStart = pkgs.writeShellScript "bgchange-${m.name}" ''
+                                    ${config.services.swww.package}/bin/swww img -o ${m.name} --resize=crop --transition-type=fade $(find ${pkgs.wallpapers.wallpapers}/share/wallpapers/${orientation} | grep -P '(png|jpg|jpeg)' | shuf -n 1)
+                                '';
+                                ExecReload = pkgs.writeShellScript "bgchange-${m.name}" ''
+                                    ${config.services.swww.package}/bin/swww img -o ${m.name} --resize=crop --transition-type=fade $(find ${pkgs.wallpapers.wallpapers}/share/wallpapers/${orientation} | grep -P '(png|jpg|jpeg)' | shuf -n 1)
+                                '';
+                                ExecStopPost = pkgs.writeShellScript "bgchange-${m.name}-clear" ''
+                                    ${config.services.swww.package}/bin/swww clear -o ${m.name}
+                                '';
                                 Type = "oneshot";
                                 RemainAfterExit = true;
                                 Restart = "on-failure";
                                 RestartSec = "5";
                             };
                             Unit = {
+                                After = target;
+                                ConditionEnvironment = "WAYLAND_DISPLAY";
                                 Description = "Change background";
+                                PartOf = target;
                             };
                         }
                     else
                         { };
-            }) (config.monitors.outputs)) //
-            builtins.listToAttrs
-            (
-                (builtins.map (m: {
-                    name = "bgchange-${m.name}-restart";
-                    value = {
-                        Service = {
-                            ExecStart = "systemctl --user ${if m.wallpaper == "swww" then "reload" else "restart"} bgchange-${m.name}.service";
-                            Type = "oneshot";
-                            Restart = "on-failure";
-                            RestartSec = "5";
-                        };
-                    };
-                }) (config.monitors.outputs))
-            )
+            }) (config.monitors.outputs)
+        )
+        // builtins.listToAttrs (
+            (builtins.map (m: {
+                name = "bgchange-${m.name}-restart";
+                value =
+                    if m.wallpaper == "swww" || m.wallpaper == "mpvpaper" then
+                        {
+                            Service = {
+                                ExecStart = pkgs.writeShellScript "bgchange-${m.name}-restart" ''
+                                    if systemctl --user is-active --quiet bgchange-${m.name}.service; then
+                                        systemctl --user reload bgchange-${m.name}.service
+                                    else
+                                        systemctl --user start bgchange-${m.name}.service
+                                    fi
+                                '';
+                                Type = "oneshot";
+                                Restart = "on-failure";
+                                RestartSec = "5";
+                            };
+                        }
+                    else
+                        { };
+            }) (config.monitors.outputs))
+        )
     );
     timers = (
         builtins.listToAttrs (
