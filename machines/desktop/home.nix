08fc05161ee6e550fa06d2d420edbc8c86ba1e4e
Emil Wendin <nudelkurre@protonmail.com>
2026-06-08 23:58:56 +0200

    Removed hyprland desktop portal

--- machines/desktop/home.nix
+++ machines/desktop/home.nix
@@ -670,7 +670,6 @@ in
             extraPortals = [
                 pkgs.xdg-desktop-portal-gnome
                 pkgs.xdg-desktop-portal-gtk
-                pkgs.xdg-desktop-portal-hyprland
                 pkgs.xdg-desktop-portal-wlr
             ];
             xdgOpenUsePortal = true;
